2450193390b36724163efafa2435f966eb12530b
wasabina67 <wasabina67@gmail.com>
2024-09-05 16:53:55 +0900

    Update Time.js

--- src/Time.js
+++ src/Time.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 function Clock() {
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [time, setTime] = useState(new Date().toLocaleTimeString('en-GB', { timeZone: 'UTC' }));
 
   useEffect(() => {
     const timerId = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
+      setTime(new Date().toLocaleTimeString('en-GB', { timeZone: 'UTC' }));
     }, 1000);
 
     return () => clearInterval(timerId);
