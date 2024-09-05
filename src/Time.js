497d38121ecd24008a8affa17a42179c49c34113
wasabina67 <wasabina67@gmail.com>
2024-09-05 16:53:51 +0900

    Update Time.js

---
+++ src/Time.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  }, []);
+
+  return (
+    <div>
+      <h1>{time}</h1>
+    </div>
+  );
+}
+
+export default Clock;
